a54c36564f134139a9fc923aa62c24dbb43adff5
CZandt <95643051+CZandt@users.noreply.github.com>
2024-02-26 12:41:13 -0700

    Update DataExploration.R
    Added dataset in this

--- DataExploration.R
+++ DataExploration.R
@@ -1,1 +1,8 @@
 ##Test file
+
+library(tidyverse)
+library(lubridate)
+library(janitor)
+library(ggplot2)
+
+data <- read_csv('https://www.dropbox.com/scl/fi/steivcw21cotncwx8tg46/20_train.csv?rlkey=a8x29lfr17pl0dbcsanx7b2qj&dl=1')
